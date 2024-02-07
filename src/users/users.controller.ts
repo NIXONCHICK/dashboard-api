608bbdd649a44233281680c864141a5a3fdd8954
NIXONCHICK <nik.nikitin990@yandex.ru>
2024-02-07 18:58:31 +0300

    hj

---
+++ src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+import { BaseController } from '../common/base.controller';
+import { HTTPError } from '../errors/http-error.class';
+import { inject, injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
+import 'reflect-metadata';
+import { IUserController } from './users.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { IUserService } from './users.service.interface';
+import { ValidateMiddleware } from '../common/validate.middleware';
+
+@injectable()
+export class UserController extends BaseController implements IUserController {
+	constructor(
+		@inject(TYPES.ILogger) public loggerService: ILogger,
+		@inject(TYPES.UserService) public userService: IUserService,
+	) {
+		super(loggerService);
+		this.bindRoutes([
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+			},
+		]);
+	}
+
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
+		next(new HTTPError(401, 'ошибка авторизации'));
+	}
+
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.createUser(body);
+		if (!result) {
+			return next(new HTTPError(422, 'Такой пользователь уже существует'));
+		}
+		this.ok(res, { email: result.email, id: result.id });
+	}
+}
